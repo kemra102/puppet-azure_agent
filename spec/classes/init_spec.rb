89d01803a1f948c9ffed3f558f5e664d5c11d2d6
Danny Roberts <danny@thefallenphoenix.net>
2015-05-25 23:30:37 +0100

    Tests written

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,34 @@
 require 'spec_helper'
-describe 'azure_agent' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('azure_agent') }
+describe 'azure_agent', :type => :class do
+  context 'default parameters on Ubuntu' do
+    let (:facts) {{ :operatingsystem => 'Ubuntu' }}
+    it {
+      should contain_class('azure_agent')
+      should contain_package('walinuxagent').with_ensure('present')
+      should contain_file('/etc/waagent.conf').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        'content' => /Provisioning.Enabled=n/,
+        'content' => /ResourceDisk.Filesystem=ext4/,
+      })
+      should contain_service('walinuxagent').with({
+        'ensure'     => 'running',
+        'enable'     => 'true',
+        'hasstatus'  => 'true',
+        'hasrestart' => 'true',
+      })
+    }
+  end
+  context 'default parameters on SLES' do
+    let (:facts) {{ :operatingsystem => 'SLES' }}
+    it {
+      should contain_package('WALinuxAgent')
+      should contain_file('/etc/waagent.conf').with({
+        'content' => /Provisioning.Enabled=y/,
+        'content' => /ResourceDisk.Filesystem=ext3/,
+      })
+    }
   end
 end
